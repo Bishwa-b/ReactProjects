e9fc4661c66dd39c7785780e2a90f39bb1b75acb
Bishwa-b <93580657+Bishwa-b@users.noreply.github.com>
2025-01-30 17:36:30 +0530

    Create App.jsx

---
+++ ReactDemo/ReactDay3/second/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from './components/Navbar.jsx';
+import Main from './components/Main.jsx';
+import './index.css';
+
+function App() {
+  return (
+    <div className="mainContiner">
+      <Navbar />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
